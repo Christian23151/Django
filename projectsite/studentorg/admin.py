e3cd5faa3efaa601563fb488dc06261811d36985
Christian23151 <202280096@psu.palawan.edu.ph>
2025-04-03 23:28:00 +0800

    PSUSphere

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+
+# Register your models here.
 from .models import College, Program, Organization, Student, OrgMember
 
 admin.site.register(College)
@@ -8,12 +10,12 @@ admin.site.register(Organization)
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     list_display = ("student_id", "lastname", "firstname", "middlename", "program")
-    search_fields = ("lastname", "firstname",)
+    search_fields = ("lastname", "firstname")
 
 @admin.register(OrgMember)
 class OrgMemberAdmin(admin.ModelAdmin):
-    list_display = ("student", "get_member_program", "organization", "date_joined",)
-    search_fields = ("student__lastname", "student__firstname",)
+    list_display = ("student", "get_member_program", "organization", "date_joined")
+    search_fields = ("student__lastname", "student__firstname")
 
     def get_member_program(self, obj):
         try:
